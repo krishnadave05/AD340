dd213ddbd7b028950b7e7b8fd6cb5afdf96b83bf
krishna Dave <82129655+krishnadave05@users.noreply.github.com>
2022-06-07 11:37:05 -0700

    Updated Circle Ci

--- app/src/androidTest/java/com/example/helloworld/MainActivityTest.java
+++ app/src/androidTest/java/com/example/helloworld/MainActivityTest.java
@@ -26,197 +26,182 @@ public class MainActivityTest {
     public ActivityScenarioRule<MainActivity> mainActivityRule =
             new ActivityScenarioRule<>(MainActivity.class);
 
-//    @Test
-//    public void checkWithMissingUsername() {
-//
-//        onView(withId(R.id.et_full_name)).perform(replaceText("Krishna Dave"), closeSoftKeyboard());
-//        onView(withId(R.id.et_email_addr)).perform(replaceText("krishnadave@gmail.com"), closeSoftKeyboard());
-//        onView(withId(R.id.et_bio)).perform(replaceText("Hey, I am ready for friendship."), closeSoftKeyboard());
-//        onView(withId(R.id.et_age)).perform(replaceText("23"), closeSoftKeyboard());
-//        onView(withId(R.id.et_occupation)).perform(replaceText("Software Developer"), closeSoftKeyboard());
-//        onView(withId(R.id.et_mobile)).perform(replaceText("9929988288"), closeSoftKeyboard());
-//
-//        onView(withId(R.id.btn_register))
-//                .perform(scrollTo(), click());
-//
-//        onView(withText("krishna555")).check(doesNotExist());
-//
-//    }
-//
-//    @Test
-//    public void checkWithWrongUsername() {
-//
-//        onView(withId(R.id.et_full_name)).perform(replaceText("Krishna Dave"), closeSoftKeyboard());
-//        onView(withId(R.id.et_email_addr)).perform(replaceText("krishnadave@gmail.com"), closeSoftKeyboard());
-//        onView(withId(R.id.et_bio)).perform(replaceText("Hey, I am ready for friendship."), closeSoftKeyboard());
-//        onView(withId(R.id.et_user_name)).perform(replaceText("krishna555"), closeSoftKeyboard());
-//        onView(withId(R.id.et_age)).perform(replaceText("23"), closeSoftKeyboard());
-//        onView(withId(R.id.et_occupation)).perform(replaceText("Software Developer"), closeSoftKeyboard());
-//        onView(withId(R.id.et_mobile)).perform(replaceText("9929988288"), closeSoftKeyboard());
-//
-//        onView(withId(R.id.btn_register))
-//                .perform(scrollTo(), click());
-//
-//        onView(withText("krishna111")).check(doesNotExist());
-//
-//    }
-//
-//    @Test
-//    public void checkWithMissingName() {
-//
-//        onView(withId(R.id.et_email_addr)).perform(replaceText("krishnadave@gmail.com"), closeSoftKeyboard());
-//        onView(withId(R.id.et_bio)).perform(replaceText("Hey, I am ready for friendship."), closeSoftKeyboard());
-//        onView(withId(R.id.et_user_name)).perform(replaceText("krishna111"), closeSoftKeyboard());
-//        onView(withId(R.id.et_age)).perform(replaceText("23"), closeSoftKeyboard());
-//        onView(withId(R.id.et_occupation)).perform(replaceText("Software Developer"), closeSoftKeyboard());
-//        onView(withId(R.id.et_mobile)).perform(replaceText("9929988288"), closeSoftKeyboard());
-//
-//        onView(withId(R.id.btn_register))
-//                .perform(scrollTo(), click());
-//
-//        onView(withText("Krishna Dave")).check(doesNotExist());
-//
-//    }
+
+
+    @Test
+    public void checkWithWrongUsername() {
+
+        onView(withId(R.id.et_full_name)).perform(replaceText("Krishna Dave"), closeSoftKeyboard());
+        onView(withId(R.id.et_email_addr)).perform(replaceText("krishnadave@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.et_bio)).perform(replaceText("Hey, I am ready for friendship."), closeSoftKeyboard());
+        onView(withId(R.id.et_user_name)).perform(replaceText("krishna555"), closeSoftKeyboard());
+        onView(withId(R.id.et_age)).perform(replaceText("23"), closeSoftKeyboard());
+        onView(withId(R.id.et_occupation)).perform(replaceText("Software Developer"), closeSoftKeyboard());
+        onView(withId(R.id.et_mobile)).perform(replaceText("9929988288"), closeSoftKeyboard());
+
+        onView(withId(R.id.btn_register))
+                .perform(scrollTo(), click());
+
+        onView(withText("krishna111")).check(doesNotExist());
+
+    }
+
+    @Test
+    public void checkWithMissingName() {
+
+        onView(withId(R.id.et_email_addr)).perform(replaceText("krishnadave@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.et_bio)).perform(replaceText("Hey, I am ready for friendship."), closeSoftKeyboard());
+        onView(withId(R.id.et_user_name)).perform(replaceText("krishna111"), closeSoftKeyboard());
+        onView(withId(R.id.et_age)).perform(replaceText("23"), closeSoftKeyboard());
+        onView(withId(R.id.et_occupation)).perform(replaceText("Software Developer"), closeSoftKeyboard());
+        onView(withId(R.id.et_mobile)).perform(replaceText("9929988288"), closeSoftKeyboard());
+
+        onView(withId(R.id.btn_register))
+                .perform(scrollTo(), click());
+
+        onView(withText("Krishna Dave")).check(doesNotExist());
+
+    }
 
 
 
-//    @Test
-//    public void checkWithMissingEmail() {
-//
-//        onView(withId(R.id.et_full_name)).perform(replaceText("Krishna Dave"), closeSoftKeyboard());
-//        onView(withId(R.id.et_bio)).perform(replaceText("Hey, I am ready for friendship."), closeSoftKeyboard());
-//        onView(withId(R.id.et_user_name)).perform(replaceText("krishna111"), closeSoftKeyboard());
-//        onView(withId(R.id.et_age)).perform(replaceText("23"), closeSoftKeyboard());
-//        onView(withId(R.id.et_occupation)).perform(replaceText("Software Developer"), closeSoftKeyboard());
-//        onView(withId(R.id.et_mobile)).perform(replaceText("9929988288"), closeSoftKeyboard());
-//
-//        onView(withId(R.id.btn_register))
-//                .perform(scrollTo(), click());
-//
-//        onView(withText("krishnadave@gmail.com")).check(doesNotExist());
-//
-//    }
-//
-//    @Test
-//    public void checkWithWrongEmail() {
-//
-//        onView(withId(R.id.et_full_name)).perform(replaceText("Krishna Dave"), closeSoftKeyboard());
-//        onView(withId(R.id.et_email_addr)).perform(replaceText("krishnadave1234@gmail.com"), closeSoftKeyboard());
-//        onView(withId(R.id.et_bio)).perform(replaceText("Hey, I am ready for friendship."), closeSoftKeyboard());
-//        onView(withId(R.id.et_user_name)).perform(replaceText("krishna111"), closeSoftKeyboard());
-//        onView(withId(R.id.et_age)).perform(replaceText("23"), closeSoftKeyboard());
-//        onView(withId(R.id.et_occupation)).perform(replaceText("Software Developer"), closeSoftKeyboard());
-//        onView(withId(R.id.et_mobile)).perform(replaceText("9929988288"), closeSoftKeyboard());
-//
-//        onView(withId(R.id.btn_register))
-//                .perform(scrollTo(), click());
-//
-//        onView(withText("krishnadave@gmail.com")).check(doesNotExist());
-//
-//    }
+    @Test
+    public void checkWithMissingEmail() {
 
-//    @Test
-//    public void checkWithMissingBio() {
-//
-//        onView(withId(R.id.et_full_name)).perform(replaceText("Krishna Dave"), closeSoftKeyboard());
-//        onView(withId(R.id.et_email_addr)).perform(replaceText("krishnadave1234@gmail.com"), closeSoftKeyboard());
-//        onView(withId(R.id.et_user_name)).perform(replaceText("krishna111"), closeSoftKeyboard());
-//        onView(withId(R.id.et_age)).perform(replaceText("23"), closeSoftKeyboard());
-//        onView(withId(R.id.et_occupation)).perform(replaceText("Software Developer"), closeSoftKeyboard());
-//        onView(withId(R.id.et_mobile)).perform(replaceText("9929988288"), closeSoftKeyboard());
-//
-//        onView(withId(R.id.btn_register))
-//                .perform(scrollTo(), click());
-//
-//        onView(withText("Hey, I am ready for friendship.")).check(doesNotExist());
-//
-//    }
-//
-//    @Test
-//    public void checkWithMissingAge() {
-//
-//        onView(withId(R.id.et_full_name)).perform(replaceText("Krishna Dave"), closeSoftKeyboard());
-//        onView(withId(R.id.et_email_addr)).perform(replaceText("krishnadave1234@gmail.com"), closeSoftKeyboard());
-//        onView(withId(R.id.et_bio)).perform(replaceText("Hey, I am ready for friendship."), closeSoftKeyboard());
-//        onView(withId(R.id.et_user_name)).perform(replaceText("krishna111"), closeSoftKeyboard());
-//        onView(withId(R.id.et_occupation)).perform(replaceText("Software Developer"), closeSoftKeyboard());
-//        onView(withId(R.id.et_mobile)).perform(replaceText("9929988288"), closeSoftKeyboard());
-//
-//        onView(withId(R.id.btn_register))
-//                .perform(scrollTo(), click());
-//
-//        onView(withText("23")).check(doesNotExist());
-//
-//    }
-//
-//    @Test
-//    public void checkWithWrongAge() {
-//
-//        onView(withId(R.id.et_full_name)).perform(replaceText("Krishna Dave"), closeSoftKeyboard());
-//        onView(withId(R.id.et_email_addr)).perform(replaceText("krishnadave1234@gmail.com"), closeSoftKeyboard());
-//        onView(withId(R.id.et_bio)).perform(replaceText("Hey, I am ready for friendship."), closeSoftKeyboard());
-//        onView(withId(R.id.et_user_name)).perform(replaceText("krishna111"), closeSoftKeyboard());
-//        onView(withId(R.id.et_age)).perform(replaceText("12"), closeSoftKeyboard());
-//        onView(withId(R.id.et_occupation)).perform(replaceText("Software Developer"), closeSoftKeyboard());
-//        onView(withId(R.id.et_mobile)).perform(replaceText("9929988288"), closeSoftKeyboard());
-//
-//        onView(withId(R.id.btn_register))
-//                .perform(scrollTo(), click());
-//
-//        onView(withText("23")).check(doesNotExist());
-//
-//    }
-//
-//    @Test
-//    public void checkWithMissingOccupation() {
-//
-//        onView(withId(R.id.et_full_name)).perform(replaceText("Krishna Dave"), closeSoftKeyboard());
-//        onView(withId(R.id.et_email_addr)).perform(replaceText("krishnadave1234@gmail.com"), closeSoftKeyboard());
-//        onView(withId(R.id.et_bio)).perform(replaceText("Hey, I am ready for friendship."), closeSoftKeyboard());
-//        onView(withId(R.id.et_user_name)).perform(replaceText("krishna111"), closeSoftKeyboard());
-//        onView(withId(R.id.et_age)).perform(replaceText("12"), closeSoftKeyboard());
-//        onView(withId(R.id.et_mobile)).perform(replaceText("9929988288"), closeSoftKeyboard());
-//
-//        onView(withId(R.id.btn_register))
-//                .perform(scrollTo(), click());
-//
-//        onView(withText("Software Developer")).check(doesNotExist());
-//
-//    }
-//
-//    @Test
-//    public void checkWithMissingMobileNumber() {
-//
-//        onView(withId(R.id.et_full_name)).perform(replaceText("Krishna Dave"), closeSoftKeyboard());
-//        onView(withId(R.id.et_email_addr)).perform(replaceText("krishnadave1234@gmail.com"), closeSoftKeyboard());
-//        onView(withId(R.id.et_bio)).perform(replaceText("Hey, I am ready for friendship."), closeSoftKeyboard());
-//        onView(withId(R.id.et_user_name)).perform(replaceText("krishna111"), closeSoftKeyboard());
-//        onView(withId(R.id.et_age)).perform(replaceText("12"), closeSoftKeyboard());
-//        onView(withId(R.id.et_occupation)).perform(replaceText("Software Developer"), closeSoftKeyboard());
-//
-//        onView(withId(R.id.btn_register))
-//                .perform(scrollTo(), click());
-//
-//        onView(withText("9929988288")).check(doesNotExist());
-//
-//    }
-//
-//    @Test
-//    public void checkWithWrongMobileNumber() {
-//
-//        onView(withId(R.id.et_full_name)).perform(replaceText("Krishna Dave"), closeSoftKeyboard());
-//        onView(withId(R.id.et_email_addr)).perform(replaceText("krishnadave1234@gmail.com"), closeSoftKeyboard());
-//        onView(withId(R.id.et_bio)).perform(replaceText("Hey, I am ready for friendship."), closeSoftKeyboard());
-//        onView(withId(R.id.et_user_name)).perform(replaceText("krishna111"), closeSoftKeyboard());
-//        onView(withId(R.id.et_age)).perform(replaceText("12"), closeSoftKeyboard());
-//        onView(withId(R.id.et_occupation)).perform(replaceText("Software Developer"), closeSoftKeyboard());
-//        onView(withId(R.id.et_mobile)).perform(replaceText("99299882889999"), closeSoftKeyboard());
-//
-//        onView(withId(R.id.btn_register))
-//                .perform(scrollTo(), click());
-//
-//        onView(withText("9929988288")).check(doesNotExist());
-//
-//    }
+        onView(withId(R.id.et_full_name)).perform(replaceText("Krishna Dave"), closeSoftKeyboard());
+        onView(withId(R.id.et_bio)).perform(replaceText("Hey, I am ready for friendship."), closeSoftKeyboard());
+        onView(withId(R.id.et_user_name)).perform(replaceText("krishna111"), closeSoftKeyboard());
+        onView(withId(R.id.et_age)).perform(replaceText("23"), closeSoftKeyboard());
+        onView(withId(R.id.et_occupation)).perform(replaceText("Software Developer"), closeSoftKeyboard());
+        onView(withId(R.id.et_mobile)).perform(replaceText("9929988288"), closeSoftKeyboard());
+
+        onView(withId(R.id.btn_register))
+                .perform(scrollTo(), click());
+
+        onView(withText("krishnadave@gmail.com")).check(doesNotExist());
+
+    }
+
+    @Test
+    public void checkWithWrongEmail() {
+
+        onView(withId(R.id.et_full_name)).perform(replaceText("Krishna Dave"), closeSoftKeyboard());
+        onView(withId(R.id.et_email_addr)).perform(replaceText("krishnadave1234@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.et_bio)).perform(replaceText("Hey, I am ready for friendship."), closeSoftKeyboard());
+        onView(withId(R.id.et_user_name)).perform(replaceText("krishna111"), closeSoftKeyboard());
+        onView(withId(R.id.et_age)).perform(replaceText("23"), closeSoftKeyboard());
+        onView(withId(R.id.et_occupation)).perform(replaceText("Software Developer"), closeSoftKeyboard());
+        onView(withId(R.id.et_mobile)).perform(replaceText("9929988288"), closeSoftKeyboard());
+
+        onView(withId(R.id.btn_register))
+                .perform(scrollTo(), click());
+
+        onView(withText("krishnadave@gmail.com")).check(doesNotExist());
+
+    }
+
+    @Test
+    public void checkWithMissingBio() {
+
+        onView(withId(R.id.et_full_name)).perform(replaceText("Krishna Dave"), closeSoftKeyboard());
+        onView(withId(R.id.et_email_addr)).perform(replaceText("krishnadave1234@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.et_user_name)).perform(replaceText("krishna111"), closeSoftKeyboard());
+        onView(withId(R.id.et_age)).perform(replaceText("23"), closeSoftKeyboard());
+        onView(withId(R.id.et_occupation)).perform(replaceText("Software Developer"), closeSoftKeyboard());
+        onView(withId(R.id.et_mobile)).perform(replaceText("9929988288"), closeSoftKeyboard());
+
+        onView(withId(R.id.btn_register))
+                .perform(scrollTo(), click());
+
+        onView(withText("Hey, I am ready for friendship.")).check(doesNotExist());
+
+    }
+
+    @Test
+    public void checkWithMissingAge() {
+
+        onView(withId(R.id.et_full_name)).perform(replaceText("Krishna Dave"), closeSoftKeyboard());
+        onView(withId(R.id.et_email_addr)).perform(replaceText("krishnadave1234@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.et_bio)).perform(replaceText("Hey, I am ready for friendship."), closeSoftKeyboard());
+        onView(withId(R.id.et_user_name)).perform(replaceText("krishna111"), closeSoftKeyboard());
+        onView(withId(R.id.et_occupation)).perform(replaceText("Software Developer"), closeSoftKeyboard());
+        onView(withId(R.id.et_mobile)).perform(replaceText("9929988288"), closeSoftKeyboard());
+
+        onView(withId(R.id.btn_register))
+                .perform(scrollTo(), click());
+
+        onView(withText("23")).check(doesNotExist());
+
+    }
+
+    @Test
+    public void checkWithWrongAge() {
+
+        onView(withId(R.id.et_full_name)).perform(replaceText("Krishna Dave"), closeSoftKeyboard());
+        onView(withId(R.id.et_email_addr)).perform(replaceText("krishnadave1234@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.et_bio)).perform(replaceText("Hey, I am ready for friendship."), closeSoftKeyboard());
+        onView(withId(R.id.et_user_name)).perform(replaceText("krishna111"), closeSoftKeyboard());
+        onView(withId(R.id.et_age)).perform(replaceText("12"), closeSoftKeyboard());
+        onView(withId(R.id.et_occupation)).perform(replaceText("Software Developer"), closeSoftKeyboard());
+        onView(withId(R.id.et_mobile)).perform(replaceText("9929988288"), closeSoftKeyboard());
+
+        onView(withId(R.id.btn_register))
+                .perform(scrollTo(), click());
+
+        onView(withText("23")).check(doesNotExist());
+
+    }
+
+    @Test
+    public void checkWithMissingOccupation() {
+
+        onView(withId(R.id.et_full_name)).perform(replaceText("Krishna Dave"), closeSoftKeyboard());
+        onView(withId(R.id.et_email_addr)).perform(replaceText("krishnadave1234@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.et_bio)).perform(replaceText("Hey, I am ready for friendship."), closeSoftKeyboard());
+        onView(withId(R.id.et_user_name)).perform(replaceText("krishna111"), closeSoftKeyboard());
+        onView(withId(R.id.et_age)).perform(replaceText("12"), closeSoftKeyboard());
+        onView(withId(R.id.et_mobile)).perform(replaceText("9929988288"), closeSoftKeyboard());
+
+        onView(withId(R.id.btn_register))
+                .perform(scrollTo(), click());
+
+        onView(withText("Software Developer")).check(doesNotExist());
+
+    }
+
+    @Test
+    public void checkWithMissingMobileNumber() {
+
+        onView(withId(R.id.et_full_name)).perform(replaceText("Krishna Dave"), closeSoftKeyboard());
+        onView(withId(R.id.et_email_addr)).perform(replaceText("krishnadave1234@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.et_bio)).perform(replaceText("Hey, I am ready for friendship."), closeSoftKeyboard());
+        onView(withId(R.id.et_user_name)).perform(replaceText("krishna111"), closeSoftKeyboard());
+        onView(withId(R.id.et_age)).perform(replaceText("12"), closeSoftKeyboard());
+        onView(withId(R.id.et_occupation)).perform(replaceText("Software Developer"), closeSoftKeyboard());
+
+        onView(withId(R.id.btn_register))
+                .perform(scrollTo(), click());
+
+        onView(withText("9929988288")).check(doesNotExist());
+
+    }
+
+    @Test
+    public void checkWithWrongMobileNumber() {
+
+        onView(withId(R.id.et_full_name)).perform(replaceText("Krishna Dave"), closeSoftKeyboard());
+        onView(withId(R.id.et_email_addr)).perform(replaceText("krishnadave1234@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.et_bio)).perform(replaceText("Hey, I am ready for friendship."), closeSoftKeyboard());
+        onView(withId(R.id.et_user_name)).perform(replaceText("krishna111"), closeSoftKeyboard());
+        onView(withId(R.id.et_age)).perform(replaceText("12"), closeSoftKeyboard());
+        onView(withId(R.id.et_occupation)).perform(replaceText("Software Developer"), closeSoftKeyboard());
+        onView(withId(R.id.et_mobile)).perform(replaceText("99299882889999"), closeSoftKeyboard());
+
+        onView(withId(R.id.btn_register))
+                .perform(scrollTo(), click());
+
+        onView(withText("9929988288")).check(doesNotExist());
+
+    }
 
 }
